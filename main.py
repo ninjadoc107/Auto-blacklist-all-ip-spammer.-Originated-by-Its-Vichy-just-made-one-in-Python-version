9584b812b5ad9015571a8b450e00bc7183652597
Bernward Sanchez <109844019+pneb@users.noreply.github.com>
2022-10-20 07:52:15 +0800

    Made it a class code
    cuz yes

--- main.py
+++ main.py
@@ -2,51 +2,53 @@ import re
 import httpx
 import asyncio
 
-max_conn = 5
-blacklisted = []
-found_address = []
+class UFW:
+    def __init__(self):
+        self.max_conn = 5
+        self.blacklisted = []
+        self.found_address = []
 
-async def juge_spam(match, list):
-    count = 0
+    async def juge_spam(self, match, list):
+        count = 0
 
-    for item in list:
-        if match == item:
-            count += 1
+        for item in list:
+            if match == item:
+                count += 1
 
-    if count >= max_conn:
-        blacklisted.append(match)
-        return True
-    else:
+        if count >= self.max_conn:
+            self.blacklisted.append(match)
+            return True
+        else:
+            return False
+
+    async def is_blacklisted(self, match):
+        for item in self.blacklisted:
+            if match == item:
+                return True
+
         return False
 
-async def is_blacklisted(match):
-    for item in blacklisted:
-        if match == item:
-            return True
+    async def blacklist(self, host, text):
+        for addr in re.findall(r'((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)', text):
+            if addr != host or not await self.is_blacklisted(addr):
+                if await self.juge_spam(addr, self.found_address):
+                    print(f"> Blacklist address --> {addr}")
+                    return
 
-    return False
+                self.found_address.append(addr)
 
-async def blacklist(host, text):
-    for addr in re.findall(r'((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)', text):
-        if addr != host or not await is_blacklisted(addr):
-            if await juge_spam(addr, found_address):
-                print(f"> Blacklist address --> {addr}")
-                return
+    async def resolve_host(self):
+        async with httpx.AsyncClient() as client:
+            res = await client.get("https://api.ipify.org")
+            return res.text
 
-            found_address.append(addr)
+    async def main(self):
+        print("|*> UFW-Firewall (PY) - github.com/pneb (originated by Its-Vichy)")
+        host = await self.resolve_host()
 
-async def resolve_host():
-    async with httpx.AsyncClient() as client:
-        res = await client.get("https://api.ipify.org")
-        return res.text
+        while True:
+            text = input()
+            await self.blacklist(host, text)
+            await asyncio.sleep(1)
 
-async def main():
-    print("|*> UFW-Firewall (PY) - github.com/pneb (originated by Its-Vichy)")
-    host = await resolve_host()
-
-    while True:
-        text = input()
-        await blacklist(host, text)
-        await asyncio.sleep(1)
-
-asyncio.run(main())
+asyncio.run(UFW().main())
